5ed5ba22b4b2af5e5c1404b1a9ec79b82465285f
shekharhinal <shekharhinal@shekharhinals-MacBook-Pro.local>
2025-12-30 20:03:55 +0530

    Day1 commit

--- practice.py
+++ practice.py
@@ -38,3 +38,49 @@ print("Last word:" , words[-1:])
 text = "   Some spaces around   "
 stripped_text = text.strip()
 print("Stripped text:", stripped_text)
+
+#string substring
+count=0
+text = "Python is awesome. Python is used widely.Python is easy language is"
+substring = "is"
+if substring in text:
+    print(substring, "found in the text")
+    count = text.count(substring)
+    print(count)
+
+# Float variables
+num1 = 5.0
+num2 = 2.5
+
+# Basic Arithmetic
+result1 = num1 + num2
+print("Addition:", result1)
+
+result2 = num1 - num2
+print("Subtraction:", result2)
+
+result3 = num1 * num2
+print("Multiplication:", result3)
+
+result4 = num1 / num2
+print("Division:", result4)
+
+# Rounding
+result5 = round(3.14159265359, 2)  # Rounds to 2 decimal places
+print("Rounded:", result5)
+
+# Integer variables
+num1 = 10
+num2 = 5
+
+# Integer Division
+result1 = num1 // num2
+print("Integer Division:", result1)
+
+# Modulus (Remainder)
+result2 = num1 % num2
+print("Modulus (Remainder):", result2)
+
+# Absolute Value
+result3 = abs(-7)
+print("Absolute Value:", result3)
